065c37eb9687e2d43611f249baf92a6eed0b481f
samuellihn <samuellihn@gmail.com>
2021-01-09 09:14:33 -0500

    support blocc schedule

--- manual.py
+++ manual.py
@@ -10,20 +10,21 @@ def open_class():
     with open("classes.csv", "r") as csvfile:
         reader = csv.reader(csvfile)
         currenttime = time.strftime("%H:%M")
-        is_correct_day = False
-        for day in next(reader):
-            if day == time.strftime("%a"):
-                is_correct_day = True
 
-        print(is_correct_day)
         for row in reader:
             if row == []:
                 continue
             if currenttime == row[0]:
                 print('Time correct')
-                if is_correct_day:
-                    print("Opening...")
-                    if row[1].startswith("https://") or row[1].startswith("http://"):
-                        webbrowser.get("\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\" %s").open(row[1])
-                    else:
-                        os.system(f"\"{row[1]}\"")
+
+                is_correct_day = False
+                for day in row[2:]:
+                    if day == time.strftime("%a"):
+                        is_correct_day = True
+
+                        if is_correct_day:
+                            print("Opening...")
+                            if row[1].startswith("https://") or row[1].startswith("http://"):
+                                webbrowser.get("\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\" %s").open(row[1])
+                            else:
+                                os.system(f"\"{row[1]}\"")
